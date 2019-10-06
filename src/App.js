2ac7cd758a4287290b16339d8b72fdcebec53d16
Bhavik Gosrani <bhavik4748@gmail.com>
2019-10-06 18:28:19 -0400

    Added logic to calculate length

--- src/App.js
+++ src/App.js
@@ -2,15 +2,33 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App" >
-        <h2>React Excercise</h2>
-        <div>Enter some text:</div>
-        <input type="text"></input>
-      </div>
-    );
-  }
+
+    state = {
+        tState: [{ text: '', textLen: 0 }]
+    };
+
+
+    textChangeHandler = (event) => {
+        let t = [...this.state.tState];
+        t[0].textLen = event.target.value.length;
+        t[0].text = event.target.value;
+        this.setState({ tStage: t });
+        // console.log(t);
+    }
+
+    render() {
+        return (
+            <div className="App" >
+                <h2>React Excercise</h2>
+                <div>Enter some text:</div>
+                <input type="text"
+                    onChange={(event) => this.textChangeHandler(event)}
+                    value={this.state.tState[0].text}
+                ></input>
+                <div>Entered Text Length:{this.state.tState[0].textLen}</div>
+            </div>
+        );
+    }
 }
 
 export default App;
