7240413a3a1608ed906bdb7c89f857117adb30d1
Bhavik Gosrani <bhavik4748@gmail.com>
2019-10-06 17:44:26 -0400

    Initial checking with textbox

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App" >
+        <h2>React Excercise</h2>
+        <div>Enter some text:</div>
+        <input type="text"></input>
+      </div>
+    );
+  }
+}
+
+export default App;
