11c0d091d73ff629201bd7097791250698ca7e38
Bhavik Gosrani <bhavik4748@gmail.com>
2019-10-07 08:39:48 -0400

    Added Index as key to Char component

--- src/App.js
+++ src/App.js
@@ -38,6 +38,7 @@ class App extends Component {
                         return <Char
                             char={t}
                             click={() => this.deletePersonHandler(index)}
+                            key={index}
                         >
 
                         </Char>
